0c33f1228dfd44d9393ba7638989017283781f82
Mounika230402 <kankanampatimounika@gmail.com>
2024-08-29 08:31:11 +0530

    files

--- src/firebase.js
+++ src/firebase.js
@@ -14,14 +14,14 @@ const firebaseConfig = {
   appId: "1:625474815335:web:b47d3dc0bfa7e28170dbc2"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase
+
 const firebaseApp = initializeApp(firebaseConfig);
-// Initialize Firebase Auth provider
+
 const provider = new GoogleAuthProvider();
   
-// whenever a user interacts with the provider, we force them to select an account
+
 provider.setCustomParameters({   
     prompt : "select_account "
 });
